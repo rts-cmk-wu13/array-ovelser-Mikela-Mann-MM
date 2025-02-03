136d729227c038110a2a41a37e2cbe749b488299
mikelamann1@gmail.com <mikelamann1@gmail.com>
2025-02-03 08:49:54 +0100

    svar på opg 1 tilføjet

--- arrays.js
+++ arrays.js
@@ -1,9 +1,52 @@
-document.addEventListener("DOMContentLoaded", function() {
-    let catsArray = ["Ollie", "Sophie", "Salem", "Tiger", "Binx", "Pumpkin", "Penny", "Lenny"];
-    let dogsArray = ["Barney", "Molly", "Baxter", "Polly", "Buddy", "Suki", "Watson"];
-    let fruitsArray = ["Banana", "Orange", "Pineapple", "Mango", "Plum", "Apricot"]
-    let numbersArray = [12, 4, 45, 17, 2, 26, 7, 55, 48, 32, 21]
+document.addEventListener("DOMContentLoaded", function () {
+    let catsArray = [
+        "Ollie",
+        "Sophie",
+        "Salem",
+        "Tiger",
+        "Binx",
+        "Pumpkin",
+        "Penny",
+        "Lenny",
+    ];
+    let dogsArray = [
+        "Barney",
+        "Molly",
+        "Baxter",
+        "Polly",
+        "Buddy",
+        "Suki",
+        "Watson",
+    ];
+    let fruitsArray = [
+        "Banana",
+        "Orange",
+        "Pineapple",
+        "Mango",
+        "Plum",
+        "Apricot",
+    ];
+    let numbersArray = [12, 4, 45, 17, 2, 26, 7, 55, 48, 32, 21];
 
-    // --- Løs opgaverne herunder --- 
+    // --- opgave 1 ---
 
-}) // ends DOMContentLoaded
+    let newNumbers = numbersArray.filter((num) => num > 20);
+    console.log("Opgave 1", newNumbers);
+
+    //The filter() method of Array instances creates a shallow copy 
+    //of a portion of a given array, filtered down to just the elements 
+    //from the given array that pass the test implemented by the provided function.
+
+
+    /* uden brug af anonym funktion 
+    
+    function isGreaterThanTwenty(num) {
+        return num > 20;
+    }
+    
+    let newNumbers = numbersArray.filter(isGreaterThanTwenty);
+    
+    console.log("Opgave 1", newNumbers);*/
+
+
+}); // ends DOMContentLoaded
